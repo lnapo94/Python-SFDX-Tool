26497ec4203e8999d638ee539670a4b2f435b33c
Luca Napoletano <lnapoletano@deloitte.it>
2022-05-08 23:11:33 +0200

    Implemented first version of SFDC Retrieve

--- src/pydx.py
+++ src/pydx.py
@@ -1,5 +1,10 @@
 import click
 import os
+import time
+import io
+
+from zipfile import ZipFile
+import xml.etree.ElementTree as ET
 
 from sfdc import Sfdc
 from utils import sfdc_utils
@@ -22,22 +27,49 @@ def retrieve(username, password, packagePath, isSandbox):
   path = DEFAULT_SRC if not packagePath else packagePath
   sfdcURL = sfdc_utils.sfdc_url(isSandbox)
 
+  packageTree = ET.parse(f'{path}/package.xml')
+  packageRoot = packageTree.getroot()
+
+  packageVersion = packageRoot.find('{http://soap.sforce.com/2006/04/metadata}version').text
+
+  package_file = open('{}/package.xml'.format(path), 'r')
+  package_text = package_file.read()
+  package_file.close()
+
+  package_text = package_text.replace('<?xml version="1.0" encoding="UTF-8"?>', '')
+  package_text = package_text.replace('<Package xmlns="http://soap.sforce.com/2006/04/metadata">', '')
+  package_text = package_text.replace('<version>{}</version>'.format(packageVersion), '')
+  package_text = package_text.replace('</Package>', '')
+
   print('{:<30}{:<40}'.format(click.style('Package.xml path: ', fg='yellow'), click.style(path, fg='green')))
   print('{:<30}{:<40}'.format(click.style('SFDC URL: ', fg='yellow'), click.style(sfdcURL, fg='green')))
+  print('{:<30}{:<40}'.format(click.style('API Version: ', fg='yellow'), click.style(packageVersion, fg='green')))
   print('{:<30}{:<40}'.format(click.style('Username: ', fg='yellow'), click.style(username, fg='green')))
   print('{:<30}{:<40}'.format(click.style('Password: ', fg='yellow'), click.style(password, fg='green')))
   print('\n')
 
+  connection = Sfdc(username, password, sfdcURL, packageVersion)
 
-  connection = Sfdc(username, password, sfdcURL)
-
-  print(click.style('Connecting to SFDC...', fg='yellow'))
+  print(click.style('Connecting to SFDC...', fg='bright_black'))
   connection.login()
   print('{}{}'.format(click.style('Connected as: ', fg='yellow'), click.style(connection.username, fg='green')))
-
-  print(click.style('Submit retrieve request...', fg='yellow'))
-  async_process_id, state = connection.retrieve()
+  
+  print(click.style('Submit retrieve request...', fg='bright_black'))
+  async_process_id, state = connection.retrieve(package=package_text)
   print('{}{}'.format(click.style('Async ID: ', fg='yellow'), click.style(async_process_id, fg='green')))
+
+  retrieving_start = time.time()
+  while not connection.isRetrievingMetadata(async_process_id):
+    print(click.style('Retrieve in progress...', fg='bright_black'))
+    time.sleep(5)
+
+  print('{} {}'.format(click.style('Retrieve completed 😎', fg='green'), click.style('[Elapsed time: {}s]'.format(int(time.time() - retrieving_start)), fg='bright_black')))
+
+  archive = ZipFile(io.BytesIO(connection.getZipFile()), "r")
+
+  for file in archive.namelist():
+    if 'package.xml' not in file:
+      archive.extract(file, path=path)
 
 if __name__ == '__main__':
   main()
--- src/sfdc/sfdc.py
+++ src/sfdc/sfdc.py
@@ -1,5 +1,6 @@
 import requests
 import xml.dom.minidom
+from base64 import b64encode, b64decode
 from xml.etree import ElementTree as ET
 from .exceptions import SalesforceAuthenticationFailed
 from .soap_messages import LOGIN_MSG, DEPLOY_MSG, CHECK_DEPLOY_STATUS_MSG, RETRIEVE_MSG, CHECK_RETRIEVE_STATUS_MSG
@@ -13,13 +14,14 @@ class Sfdc:
     'mt': 'http://soap.sforce.com/2006/04/metadata'
     }
 
-  def __init__(self, username, password, url):
+  def __init__(self, username, password, url, apiVersion):
     """
     Initializes a new SFDC object to work with Salesforce Metadata API.
     """
     self.url = url
     self.username = username
     self.password = password
+    self.apiVersion = apiVersion
 
   def login(self):
     """
@@ -37,7 +39,7 @@ class Sfdc:
     login_soap_body = LOGIN_MSG.format(username=self.username, password=self.password)
 
     # Build the actual Login URL
-    login_url = f'{self.url}/services/Soap/u/54.0'
+    login_url = f'{self.url}/services/Soap/u/{self.apiVersion}'
 
     # Make the request
     response = requests.post(url=login_url, data=login_soap_body, headers=login_soap_request_headers)
@@ -46,14 +48,13 @@ class Sfdc:
       # If the request was good, parse it to obtain SessionID and ServerURL
       self.__sessionId = self.__getUniqueElementValueFromXmlString(response.text, 'sessionId')
       self.__serverUrl = self.__getUniqueElementValueFromXmlString(response.text, 'serverUrl')
-      print(f'Session ID {self.__sessionId}')
     else:
       # Othewise, throw an exception
       except_code = self.__getUniqueElementValueFromXmlString(response.content, 'sf:exceptionCode')
       except_msg = self.__getUniqueElementValueFromXmlString(response.content, 'sf:exceptionMessage')
       raise SalesforceAuthenticationFailed(response.status_code, except_msg)
 
-  def retrieve(self):
+  def retrieve(self, package):
     """
     Submit a retrieve request to SFDC Metadata API.
     """
@@ -65,24 +66,14 @@ class Sfdc:
       'SOAPAction': 'retrieve'
     }
 
-    # Get all the Metadata from the specified "package.xml" file
-    # TODO To be implemented
-    retrieve_package = """<types>
-        <members>*</members>
-        <name>Group</name>
-    </types>"""
-
     # Get the Retrieve SOAP Message and complete it with additional information
-    retrieve_soap_body = RETRIEVE_MSG.format(sessionId=self.__sessionId, apiVersion='54.0', singlePackage=True, unpackaged=retrieve_package)
+    retrieve_soap_body = RETRIEVE_MSG.format(sessionId=self.__sessionId, apiVersion=self.apiVersion, singlePackage=True, unpackaged=package)
 
     # Build the actual Retrieve URL
     retrieve_url = self.__serverUrl.replace('/u/', '/m/')
 
     # Make the request
     response = requests.post(url=retrieve_url, data=retrieve_soap_body, headers=retrieve_soap_request_headers)
-
-    print(self.__serverUrl)
-    print(response.text)
 
     # Parse response to get Async Id and its Status
     async_process_id = ET.fromstring(response.text).find(
@@ -94,12 +85,46 @@ class Sfdc:
 
     return async_process_id, status
 
-  def checkRetrieveStatus(self, asyncProcessId):
+  def isRetrievingMetadata(self, asyncProcessId):
     """
     Checks the status of a retrieve request.
     TODO: To be implemented
     """
-    pass
+
+    # Setup the headers for the request
+    retrieve_status_soap_request_headers = {
+      'content-type': 'text/xml',
+      'charset': 'UTF-8',
+      'SOAPAction': 'checkRetrieveStatus'
+    }
+
+    # Get the Retrieve SOAP Message and complete it with additional information
+    retrieve_status_soap_body = CHECK_RETRIEVE_STATUS_MSG.format(sessionId=self.__sessionId, asyncProcessId=asyncProcessId, includeZip=True)
+
+    # Build the actual Retrieve URL
+    retrieve_status_url = self.__serverUrl.replace('/u/', '/m/')
+
+    # Make the request
+    response = requests.post(url=retrieve_status_url, data=retrieve_status_soap_body, headers=retrieve_status_soap_request_headers)
+
+    # Parse response to check retrieving status
+    root = ET.fromstring(response.text)
+    result = root.find('soapenv:Body/mt:checkRetrieveStatusResponse/mt:result/mt:done', self._XML_NAMESPACES)
+
+    if result is None:
+      raise Exception(f'Result node could not be found: {response.text}')
+    elif result.text == 'true':
+      self.__retrieveResult = response.text
+      return True
+    else:
+      return False
+
+  def getZipFile(self):
+    if self.__retrieveResult is not None:
+      root = ET.fromstring(self.__retrieveResult)
+      result = root.find('soapenv:Body/mt:checkRetrieveStatusResponse/mt:result', self._XML_NAMESPACES)
+      zipfile_base64 = result.find('mt:zipFile', self._XML_NAMESPACES).text
+      return b64decode(zipfile_base64)
 
   def deploy(self):
     """
